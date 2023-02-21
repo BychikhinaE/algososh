fee4a91b401b8d40a671575720e66ce249852d0c
Katerina Bychikhina <orodiavoletto@yandex.ru>
2023-02-21 13:02:23 +0300

    first

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Button } from "../ui/button/button";
 
 export const StringComponent: React.FC = () => {
   return (
